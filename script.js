154a4c9ecbefed53844014df10e17d733c57cefc
LucasLCabral <lucasludovicocabraal@gmail.com>
2024-05-02 19:57:32 -0300

    Criando o slideShow

---
+++ script.js
@@ -0,0 +1,15 @@
+/* Comando dos slide show */
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+let slides = document.getElementsByClassName("slide");
+for (let i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";
+}
+slideIndex++;
+if (slideIndex > slides.length) {slideIndex = 1}
+slides[slideIndex-1].style.display = "block";
+setTimeout(showSlides, 20000); // Altere o valor para ajustar a velocidade do slideshow (em milissegundos)
+}
